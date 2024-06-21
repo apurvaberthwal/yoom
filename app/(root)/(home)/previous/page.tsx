14cdda0bf7ecb706127be8fa4772e3a8e4115efc
apurvaberthwal <apurva3barthwal@gmail.com>
2024-06-21 07:52:59 +0530

    first commit

---
+++ app/(root)/(home)/previous/page.tsx
@@ -0,0 +1,13 @@
+
+const Previous = () => {
+  return ( <section className="flex size-full flex-col gap-10 text-white">
+
+  <h1 className='text-3xl font-bold'>
+  PRevopus
+  </h1>
+  
+ </section>
+)
+}
+
+export default Previous
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { ClerkProvider } from "@clerk/nextjs";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -16,7 +17,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <ClerkProvider
+      appearance={{
+        layout:{
+          logoImageUrl: "/icons/yoom-logo.svg",
+          socialButtonsVariant:"iconButton"
+        },
+        variables: {
+          colorText: "#ffffff",
+          colorPrimary: "#0e78F9",
+          colorBackground: "#1C1f2e",
+          colorInputBackground: "#252a41",
+          colorInputText:"#fff"
+
+        }
+      }}>
+      <body className={` ${inter.className} bg-dark-2`}>
+        {children}
+        </body>
+      </ClerkProvider>
     </html>
   );
 }
