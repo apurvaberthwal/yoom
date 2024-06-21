3c193576df8fc2d297a41f56d8a55cdc40ab671e
apurvaberthwal <apurva3barthwal@gmail.com>
2024-06-21 21:47:15 +0530

    added homepage ui

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,13 +1,36 @@
-import React from 'react'
+import MeetingTypeList from "@/components/MeetingTypeList";
 
 const Home = () => {
-  return (
-   <section className="flex size-full flex-col gap-10 text-white">
+  const now = new Date();
+  const time = now.toLocaleTimeString("en-US", { hour: "2-digit", minute: "2-digit" });
 
-    <h1 className='text-3xl font-bold'>
-    Home
+  const date = (new Intl.DateTimeFormat("en-IN",{
+    dateStyle: "full"
+  })).format(now)
+
+
+  return (
+   <section className="flex size-full flex-col 
+   gap-10 text-white">
+
+   <div className="h-[300px] w-full 
+   rounded-[20px] bg-hero bg-cover">
+<div className="flex h-full flex-col justify-between
+ max-md:px-5 max-md:py-8 lg:p-11">
+<h2 className='glassmorphism max-w-[270px]
+ rounded text-center text-base font-normal py-2'>Upcoming meeting at 12:30 pm</h2>
+  <div className="flex flex-col gap-2">
+    <h1 className="text-4xl font-extrabold lg:text-7xl">
+     {time}
     </h1>
-    
+    <p className="text-lg font-medium text-sky-1 lg:text-2xl">
+     {date}
+    </p>
+  </div>
+</div>
+
+   </div>
+    <MeetingTypeList />
    </section>
   )
 }
